1f36fda58c0995407327da185b7084b5c66fff09
Benjamin Lenherr <sunsetroad@gmx.ch>
2026-06-15 15:16:11 +0200

    Create install_service.sh

---
+++ install_service.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Install erverpi-send-intermediate-status"
+sh ~/ServerPi/python/serverpi-send-intermediate-status/install.sh
+echo "Install serverpi-send-ready-status"
+sh ~/ServerPi/python/serverpi-send-ready-status/install.sh

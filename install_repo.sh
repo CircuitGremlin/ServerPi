a236ace890655d441ade10fa9af0e20234c06205
Benjamin Lenherr <sunsetroad@gmx.ch>
2026-06-15 15:11:55 +0200

    Create install_repo.sh

---
+++ install_repo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Go into home directory
+echo "Go into home directory"
+cd ~/
+
+# Get repo
+echo "Get 'ServerPi' repo"
+git clone https://github.com/CircuitGremlin/ServerPi.git
+
+# Open directory
+echo "Open 'ServerPi' directory"
+cd ServerPi
+
+# Create virtual python environment
+echo "Create virtual python environment"
+python3 -m venv --system-site-packages pyvenv
+
+# Activate virtual python environment
+echo "Activate virtual python environment"
+source ./pyvenv/bin/activate
+
+# Install pip packages
+echo "Install pip packages"
+pip3 install smbus2
+
+# Done
+echo "Done!"

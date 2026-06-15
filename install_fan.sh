7d39df107a895c41a7c0e0a665dc47e9d7c2f401
Benjamin Lenherr <sunsetroad@gmx.ch>
2026-06-15 17:29:37 +0200

    Create install_fan.sh

---
+++ install_fan.sh
@@ -0,0 +1,16 @@
+
+
+# Backup config.txt
+echo "Backup config.txt"
+sudo cp /boot/firmware/config.txt /boot/firmware/config.txt.bak
+
+# Add lines to '/boot/firmware/config.txt'
+echo "Add lines to '/boot/firmware/config.txt"
+sudo sh -c 'echo "# Enable case fan" >> /boot/firmware/config.txt'
+sudo sh -c 'echo "dtoverlay=pwm-gpio-fan,fan_gpio=27" >> /boot/firmware/config.txt'
+sudo sh -c '"" >> /boot/firmware/config.txt'
+sudo sh -c '"# Fan temperature settings (CPU and case fan)" >> /boot/firmware/config.txt'
+sudo sh -c '"dtparam=fan_temp0=45000" >> /boot/firmware/config.txt'
+sudo sh -c '"dtparam=fan_temp1=50000" >> /boot/firmware/config.txt'
+sudo sh -c '"dtparam=fan_temp2=55000" >> /boot/firmware/config.txt'
+sudo sh -c '"dtparam=fan_temp3=60000" >> /boot/firmware/config.txt'
